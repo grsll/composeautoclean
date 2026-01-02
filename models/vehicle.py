4386d786f01f52b2d663d772cff541bf1c6a1169
yoga684 <astdnn48@gmail.com>
2026-01-02 16:10:10 +0700

    update: menambahkan customer dan icon aplikasinya

--- models/vehicle.py
+++ models/vehicle.py
@@ -1,15 +1,25 @@
 from odoo import models, fields
 
-class Vehicle(models.Model):
-    _name = 'compose_auto_clean.vehicle'
-    _description = 'Tabel Kendaraan'
 
-    name = fields.Char(string='Plat Nomor Kendaraan', required=True)
-    vehicle_type = fields.Selection([
-        ('mobil', 'Mobil'),
-        ('motor', 'Motor')
-    ], string='Jenis Kendaraan', required=True, default='mobil')
-    brand = fields.Char(string='Merek Kendaraan')
-    color = fields.Char(string='Warna Kendaraan')
-    customer_id = fields.Many2one('res.partner', string='Pemilik Kendaraan', required=True, ondelete='cascade')
-    wash_order_ids = fields.One2many('compose_auto_clean.carwash_order', 'vehicle_id', string='Riwayat Cuci')
+class Vehicle(models.Model):
+    _name = "compose_auto_clean.vehicle"
+    _description = "Tabel Kendaraan"
+
+    name = fields.Char(string="Plat Nomor Kendaraan", required=True)
+    vehicle_type = fields.Selection(
+        [("mobil", "Mobil"), ("motor", "Motor")],
+        string="Jenis Kendaraan",
+        required=True,
+        default="mobil",
+    )
+    brand = fields.Char(string="Merek Kendaraan")
+    color = fields.Char(string="Warna Kendaraan")
+    customer_id = fields.Many2one(
+        "compose_auto_clean.customer",
+        string="Pemilik Kendaraan",
+        required=True,
+        ondelete="cascade",
+    )
+    wash_order_ids = fields.One2many(
+        "compose_auto_clean.carwash_order", "vehicle_id", string="Riwayat Cuci"
+    )
