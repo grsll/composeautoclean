7495a1cdc8393d7894f41537813323236c2b39c5
yoga684 <astdnn48@gmail.com>
2026-01-07 16:33:47 +0700

    update: tampilan kendaraan dan customer

--- models/res_partner_inherit.py
+++ models/res_partner_inherit.py
@@ -5,13 +5,13 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    nomor_kendaraan = fields.Char(string="Nomor Kendaraan")
-    jenis_kendaraan = fields.Selection(
-        selection=[("mobil", "Mobil"), ("motor", "Motor"), ("bus", "Bus")],
-        string="Jenis Kendaraan",
-    )
     carwash_order_ids = fields.One2many(
         "carwash.order",
         "partner_id",
         string="Riwayat Pesanan",
+    )
+    kendaraan_ids = fields.One2many(
+        "cdn.kendaraan",
+        "customer_id",
+        string="Daftar Kendaraan",
     )
