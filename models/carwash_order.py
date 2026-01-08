9136f8cac7b55ad2137298c27bd7a652218e95dd
Napel-coding <maheswara_dzakwan_32rpl@student.smktelkom-mlg.sch.id>
2026-01-08 09:56:06 +0700

    Modified: Tampilan Carwash Order

--- models/carwash_order.py
+++ models/carwash_order.py
@@ -45,6 +45,12 @@ class CarwashOrder(models.Model):
         readonly=True,
     )
 
+    invoice_id = fields.Many2one(
+        "account.move",
+        string="Invoice",
+        readonly=False,
+    )
+
     state = fields.Selection(
         [
             ("draft", "Draft"),
@@ -82,6 +88,8 @@ class CarwashOrder(models.Model):
         """
         Reset product ketika customer berubah
         """
+        if not hasattr(self.partner_id, 'id'):
+            self.partner_id = False
         self.product_id = False
 
     def action_draft(self):
@@ -91,4 +99,28 @@ class CarwashOrder(models.Model):
         self.state = "confirmed"
 
     def action_invoice(self):
+        account = self.product_id._get_product_accounts()['income']
+        if not account:
+            raise ValueError("Product does not have an income account set.")
+        invoice_vals = {
+            'move_type': 'out_invoice',
+            'partner_id': self.partner_id.id,
+            'invoice_date': fields.Date.today(),
+            'invoice_line_ids': [(0, 0, {
+                'product_id': self.product_id.id,
+                'quantity': 1,
+                'price_unit': self.price,
+                'account_id': account.id,
+            })],
+        }
+        invoice = self.env['account.move'].create(invoice_vals)
+        self.invoice_id = invoice.id
         self.state = "invoiced"
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move',
+            'res_id': invoice.id,
+            'view_mode': 'form',
+            'view_type': 'form',
+            'target': 'current',
+        }
