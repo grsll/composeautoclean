3584d58350038ab97f0ed8eca571d57122fe64bf
yoga684 <astdnn48@gmail.com>
2026-01-08 13:24:16 +0700

    update: merubah customer, kendaraan, carwash_order dan menambahkan state di carwash order

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import kendaraan
 from . import res_partner_inherit
 from . import carwash_order
+from . import carwash_order_state
--- models/carwash_order.py
+++ models/carwash_order.py
@@ -16,14 +16,6 @@ class CarwashOrder(models.Model):
     vehicle_id = fields.Many2one(
         "cdn.kendaraan",
         string="Kendaraan",
-        required=True,
-    )
-
-    nomor_kendaraan = fields.Char(
-        string="Nomor Kendaraan",
-        compute="_compute_customer_info",
-        store=True,
-        readonly=True,
     )
 
     jenis_kendaraan = fields.Selection(
@@ -33,10 +25,13 @@ class CarwashOrder(models.Model):
             ("bus", "Bus"),
         ],
         string="Jenis Kendaraan",
-        compute="_compute_customer_info",
-        store=True,
-        readonly=True,
+        required=True,
     )
+
+    @api.onchange("vehicle_id")
+    def _onchange_vehicle_id(self):
+        if self.vehicle_id:
+            self.jenis_kendaraan = self.vehicle_id.jenis_kendaraan
 
     product_id = fields.Many2one(
         "product.product",
@@ -68,21 +63,6 @@ class CarwashOrder(models.Model):
         readonly=True,
     )
 
-    @api.depends(
-        "vehicle_id",
-        "vehicle_id.nomor_kendaraan",
-        "vehicle_id.jenis_kendaraan",
-    )
-    def _compute_customer_info(self):
-        for rec in self:
-            vehicle = rec.vehicle_id
-            if vehicle:
-                rec.nomor_kendaraan = vehicle.nomor_kendaraan
-                rec.jenis_kendaraan = vehicle.jenis_kendaraan
-            else:
-                rec.nomor_kendaraan = False
-                rec.jenis_kendaraan = False
-
     @api.depends("product_id")
     def _compute_price(self):
         for rec in self:
@@ -103,28 +83,34 @@ class CarwashOrder(models.Model):
         self.state = "confirmed"
 
     def action_invoice(self):
-        account = self.product_id._get_product_accounts()['income']
+        account = self.product_id._get_product_accounts()["income"]
         if not account:
             raise ValueError("Product does not have an income account set.")
         invoice_vals = {
-            'move_type': 'out_invoice',
-            'partner_id': self.partner_id.id,
-            'invoice_date': fields.Date.today(),
-            'invoice_line_ids': [(0, 0, {
-                'product_id': self.product_id.id,
-                'quantity': 1,
-                'price_unit': self.price,
-                'account_id': account.id,
-            })],
+            "move_type": "out_invoice",
+            "partner_id": self.partner_id.id,
+            "invoice_date": fields.Date.today(),
+            "invoice_line_ids": [
+                (
+                    0,
+                    0,
+                    {
+                        "product_id": self.product_id.id,
+                        "quantity": 1,
+                        "price_unit": self.price,
+                        "account_id": account.id,
+                    },
+                )
+            ],
         }
-        invoice = self.env['account.move'].create(invoice_vals)
+        invoice = self.env["account.move"].create(invoice_vals)
         self.invoice_id = invoice.id
         self.state = "invoiced"
         return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'account.move',
-            'res_id': invoice.id,
-            'view_mode': 'form',
-            'view_type': 'form',
-            'target': 'current',
+            "type": "ir.actions.act_window",
+            "res_model": "account.move",
+            "res_id": invoice.id,
+            "view_mode": "form",
+            "view_type": "form",
+            "target": "current",
         }
